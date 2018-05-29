e4d8b81503ae73e9061b93d05a88f56500f7b19f
David Costa <david.costa@codeminer42.com>
2018-05-29 10:36:25 -0300

    Fix validations and has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many: :notes
-  validates: :name, presence: true, on: :update
+  has_many :notes
+  validates :name, presence: true, on: :update
 end
